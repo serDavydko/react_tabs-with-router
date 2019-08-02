c514c7d523476abd90889a2f90470f5ce07feb54
Serhii <davydko.serhiy@gmail.com>
2019-08-02 11:43:36 +0300

    Updates

---
+++ precache-manifest.6d6d398d81b7186bebf9da7c151cc7be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931176248ddd2b1c0eaba51767a526b9",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "00275d679ad75b841dc4",
+    "url": "/react_tabs-with-router/static/css/main.07a4223c.chunk.css"
+  },
+  {
+    "revision": "5789c5a7c7dafdabb00a",
+    "url": "/react_tabs-with-router/static/js/2.1b1c988e.chunk.js"
+  },
+  {
+    "revision": "00275d679ad75b841dc4",
+    "url": "/react_tabs-with-router/static/js/main.f85749fc.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
